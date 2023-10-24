32210f612dcc8d0f2760d2572b8fff051a46079e
TejeswarBobba <S567141@nwmissouri.edu>
2023-10-23 21:07:39 -0500

    user changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+more = 0;
+  sum = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more = more + 1
+  sum = sum + more
+
+  res.send(`sum is ${sum}`);
 });
 
 module.exports = router;
